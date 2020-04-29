4ac4327b89eed362ac78dc1ec0adae478cdb4190
voradhwani <voradhwani@gmail.com>
2020-04-30 01:19:32 +0530

    added create task api

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ app.use(express.json())
 
 app.get('/todo', (req, res) => res.send(tasks))
 
+app.post('/todo', (req, res) => {
+    const task = req.body.task;
+    tasks.push(task);
+    res.send(task);
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
